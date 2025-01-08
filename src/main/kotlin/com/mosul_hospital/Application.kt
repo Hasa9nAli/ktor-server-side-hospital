54f037ce5daba4564552bbc39bca8a42aaee4624
Hasan Ali <ihasanalihasan@gmail.com>
2025-01-08 22:08:39 +0300

    main file configuration

---
+++ src/main/kotlin/com/mosul_hospital/Application.kt
@@ -0,0 +1,15 @@
+package com.mosul_hospital
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
---
+++ src/main/kotlin/com/mosul_hospital/Monitoring.kt
@@ -0,0 +1,13 @@
+package com.mosul_hospital
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.calllogging.CallLogging
+import io.ktor.server.request.path
+import org.slf4j.event.Level
+
+fun Application.configureMonitoring() {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+}
