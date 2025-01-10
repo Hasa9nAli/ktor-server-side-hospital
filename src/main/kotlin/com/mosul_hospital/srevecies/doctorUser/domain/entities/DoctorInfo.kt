956914a0cfd028a6ca52c54e8bc228e7e9c5cf49
Hasan Ali <ihasanalihasan@gmail.com>
2025-01-10 22:00:39 +0300

    add entities

---
+++ src/main/kotlin/com/mosul_hospital/srevecies/doctorUser/domain/entities/DoctorInfo.kt
@@ -0,0 +1,11 @@
+package com.mosul_hospital.srevecies.doctorUser.domain.entities
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DoctorInfo(
+    val doctorId: String,
+    val doctorName: String,
+    val doctorSpecification: String,
+    val casesTreatedByThisDoctor: List<PatientCase>
+)
