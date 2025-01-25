bfaf319d262e7ceac79d7830b1cadbf1da50a761
Hasan Ali <ihasanalihasan@gmail.com>
2025-01-25 18:34:46 +0300

    add Laboratory info

---
+++ src/main/kotlin/com/mosul_hospital/srevecies/laboratory/domain/entities/LaboratoryInfo.kt
@@ -0,0 +1,7 @@
+package com.mosul_hospital.srevecies.laboratory.domain.entities
+
+data class LaboratoryInfo(
+    val laboratoryEmployeeId: String,
+    val laboratoryEmployeeName: String,
+    val laboratoryEmployeeEmail: String
+)
